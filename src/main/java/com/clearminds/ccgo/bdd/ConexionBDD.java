8b320ddb5a84027c6ebc9405a23c31d9782ece74
CristianGomezO <cristiangomezosorio17@gmail.com>
2020-12-09 10:10:56 -0500

    Corrección en el metodo TestPropiedades

--- src/main/java/com/clearminds/ccgo/bdd/ConexionBDD.java
+++ src/main/java/com/clearminds/ccgo/bdd/ConexionBDD.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConexionBDD {
+	
 	public static String leerPropiedad(String nombrePropiedad){
 		Properties propiedades = new Properties();
 		String valorPropiedad = "";
--- src/test/java/com/clearmindsccgo/bdd/test/TestPropiedades.java
+++ src/test/java/com/clearmindsccgo/bdd/test/TestPropiedades.java
@@ -5,12 +5,11 @@ import com.clearminds.ccgo.bdd.ConexionBDD;
 public class TestPropiedades {
 
 	public static void main(String[] args) {
-		ConexionBDD conexionBDD = new ConexionBDD();
 		
-		String valor = conexionBDD.leerPropiedad("propiedad1");
+		String valor = ConexionBDD.leerPropiedad("propiedad1");
 		System.out.println(valor);
 		
-		valor = conexionBDD.leerPropiedad("xx");
+		valor = ConexionBDD.leerPropiedad("xx");
 		System.out.println(valor);
 
 	}
